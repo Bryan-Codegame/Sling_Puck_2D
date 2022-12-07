436ee68b017777dd75d234255eca3d2a0c671c05
bryanLaptop <bryanesc26@gmail.com>
2022-12-06 23:12:23 -0600

    Add ShotManager to instantiate pockets and create walls

--- Assets/_Scripts/ShotManager.cs
+++ Assets/_Scripts/ShotManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ShotManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject projectile;
+    public float projectileForce = 5f;
+    public Transform startShotPoint;
+   
+    
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 projectilePos = transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 aimerDirection = mousePos - projectilePos;
+        transform.right = aimerDirection;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    void Shoot()
+    {
+        GameObject newProjectile = Instantiate(projectile, startShotPoint.position, startShotPoint.rotation);
+        newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileForce;
     }
 }
